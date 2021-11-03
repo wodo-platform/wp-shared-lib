8a7aeb98d95f25eabf95dce4e1f5c9122b997d41
Serhat Tanrikut <serhat.tanrikut@gmail.com>
2021-11-03 23:05:07 +0100

    first commit

---
+++ src/wodopaltform/crptocurrency/crpto.assets.ts
@@ -0,0 +1,10 @@
+export interface CrptoAsset {
+    id:number;
+    name:string;
+    symbol:string;
+    description:string;
+}
+
+export var WODO_COIN = {id:1, name:"Wodo Coin", symbol:"WC", description:"Wodo Platform coin"}, 
+BANANO = {id:2, name:"Banano",  symbol:"BNN", description:"Banano community coin"},
+NANO = {id:3, name:"Nano",  symbol:"Nan", description:"NANO community coin"};
